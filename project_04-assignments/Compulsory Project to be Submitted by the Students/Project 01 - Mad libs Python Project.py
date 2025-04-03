3c3c4fe7708e8a0da61c2497bcc486dd2d68770c
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-04-03 19:57:18 +0500

    completed Project 4 Assignment

---
+++ project_04-assignments/Compulsory Project to be Submitted by the Students/Project 01 - Mad libs Python Project.py
@@ -0,0 +1,16 @@
+# Project 1: Mad libs Python Project
+
+
+def simple_programming_madlib():
+    language = input("Enter a programming language: ")
+    adjective = input("Enter an adjective: ")
+    verb = input("Enter a verb (present tense): ")
+    noun = input("Enter a noun (plural): ")
+
+    story = f"Learning {language} can be very {adjective}.You need to {verb} a lot of code and debug many {noun}.But in the end, it's a {adjective} skill to have!"
+
+    print("\nYour Programming Mad Libs story:")
+    print(story)
+
+if __name__ == "__main__":
+    simple_programming_madlib()
